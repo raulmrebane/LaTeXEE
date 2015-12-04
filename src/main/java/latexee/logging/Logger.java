34bb398983f6ff3f0ac89bd73a4f97f72d2b3b05
Abercus <joonas.puura@gmail.com>
2015-12-04 14:00:30 +0200

    #65 documentation for the logging class.

--- src/main/java/latexee/logging/Logger.java
+++ src/main/java/latexee/logging/Logger.java
@@ -2,7 +2,15 @@ package main.java.latexee.logging;
 
 import main.java.latexee.runtime.Main;
 
+/**
+ * Provides logging capability for the project.
+ * Currently only supports writing to standard output.
+ */
 public class Logger {
+	/**
+	 * Method to output logging message to standard output. Currently only when the verbose flag is true.
+	 * @param message string which is output to standard output
+	 */
 	//Broken code gets changed, bad code stays forever.
 	public static void log(String message){
 		if(Main.verbose){
