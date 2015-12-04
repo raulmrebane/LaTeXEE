72c10545bc8aeed8966a157c839e12af9b1f7362
Abercus <joonas.puura@gmail.com>
2015-12-04 15:01:30 +0200

    #65 Documentation for exceptions

--- src/main/java/latexee/exceptions/TemplateFillException.java
+++ src/main/java/latexee/exceptions/TemplateFillException.java
@@ -1,9 +1,15 @@
 package main.java.latexee.exceptions;
 
+/**
+ * Exception, which is thrown when OpenMath template could not be filled correctly (missing/wrong arguments)
+ */
 public class TemplateFillException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * TemplateFillException constructor
+	 */
 	public TemplateFillException() {
 		super();
 	}
