45493c1c3caf2320c22b3eb72482501a0643d3c1
Abercus <joonas.puura@gmail.com>
2015-12-06 00:19:30 +0200

    #65 documentation for DeclarationParser.java

--- src/main/java/latexee/parsers/DeclarationParser.java
+++ src/main/java/latexee/parsers/DeclarationParser.java
@@ -22,53 +22,75 @@ import main.java.latexee.errorlisteners.DeclarationErrorListener;
 import main.java.latexee.exceptions.DeclarationInitialisationException;
 import main.java.latexee.logging.Logger;
 
+/**
+ * DeclarationParser class contains methods for finding and  parsing the operator and macro declarations.
+ * Example of a macro declaration \declare{macro=\frac, meaning=arith1.divide, argspec=[2], code={...}}
+ * Example of a operator declaration \declare{syntax={infix, 7, /, l}, meaning=arith1.divide}
+ * Declarations are after translated to grammar rules.
+ */
 public class DeclarationParser {
 	//goes through the AST and parses each DeclarationStatement. 
 	//Can be later integrated into a function that does everything in one run through the AST for performance.
+
+	/**
+	 * This method traverses the given AST and finds all declarations.
+	 * Work is delegated to different method with same signature.
+	 * @param node entry node to tree containing all statements
+	 */
 	public static void declarationFinder(ParsedStatement node){
 		declarationFinder(node, 0);
 	}
+
+	/**
+	 * Static method to find all the macro and operator declarations in the statement tree.
+	 * @param node entry node to statements branch
+	 * @param maxId amount of declarations parsed
+	 * @return integer of how many operator or macro declarations were found
+	 */
 	public static Integer declarationFinder(ParsedStatement node, Integer maxId){
 		if(node instanceof DeclareStatement){
 			DeclareStatement castNode = (DeclareStatement) node;
 			ParseTree parseTree = parseDeclaration(castNode.getContent());
-			if (parseTree != null) {
-				boolean operatorStyle = isOperatorSyntax(parseTree);
-				if (operatorStyle){
-					try {
-						Logger.log("Parsing an operator.");
-						OperatorDeclaration opDec = new OperatorDeclaration(parseTree,maxId);
-						maxId++;
-						castNode.setNode(opDec);
-						Logger.log("Parsing successful.\n");
-					}
-					catch (DeclarationInitialisationException die) {
-						Logger.log("Parsing finished with errors.\n");
-					}
+			boolean operatorStyle = isOperatorSyntax(parseTree);
+			if (operatorStyle){
+				try {
+					Logger.log("Parsing an operator."); //TODO: change?
+					OperatorDeclaration opDec = new OperatorDeclaration(parseTree,maxId);
+					maxId++;
+					castNode.setNode(opDec);
+					Logger.log("Parsing successful.\n");
 				}
-				else {
-					try {
-						Logger.log("Parsing a macro.");
-						castNode.setNode(new MacroDeclaration(parseTree,maxId));
-						maxId++;
-						Logger.log("Parsing successful.\n");
-					}
-					catch (DeclarationInitialisationException die) {
-						Logger.log("Parsing finished with errors.\n");
-					}
+				catch (DeclarationInitialisationException die) {
+					Logger.log("Parsing finished with errors.\n");
 				}
 			}
+			else {
+				try {
+					Logger.log("Parsing a macro.");
+					castNode.setNode(new MacroDeclaration(parseTree,maxId));
+					maxId++;
+					Logger.log("Parsing successful.\n");
+				}
+				catch (DeclarationInitialisationException die) {
+					Logger.log("Parsing finished with errors.\n");
+				}
+			}			
 		}
 		ArrayList<ParsedStatement> children = node.getChildren();
 		Integer childMax = maxId;
-		for(int i=0;i<children.size();i++){
-			childMax = declarationFinder(children.get(i),childMax);
+		for (ParsedStatement child : children) {
+			childMax = declarationFinder(child,childMax);
 		}
 		maxId = childMax;
 		return maxId;
 		
 	}
 
+	/**
+	 * Method to find if the declaration contains operator syntax.
+	 * @param tree statement node
+	 * @return true if node is operator statement, false if not operator then it should be macro statement
+	 */
 	public static boolean isOperatorSyntax(ParseTree tree){
 		boolean foundOperator = false;
 		if(tree instanceof SyntaxBracketContext){
@@ -84,7 +106,12 @@ public class DeclarationParser {
 	}
 	
 	//Generates the ANTLR parse tree for each declaration string
-	
+
+	/**
+	 * After we have found our declarations we will generate ANTLR parse tree for each declaration string.
+	 * @param rule declaration in string format
+	 * @return parse tree of the declaration.
+	 */
 	public static ParseTree parseDeclaration(String rule){
 		ANTLRInputStream antlrInput = new ANTLRInputStream(rule);
 	    DeclarationGrammarLexer lexer = new DeclarationGrammarLexer(antlrInput);
@@ -100,36 +127,11 @@ public class DeclarationParser {
 		lexer.addErrorListener(del);
 		
 	    ParseTree tree = parser.declarationGrammar();
-	    if (del.foundErrors()) {
+	    if (del.foundErrors())
 	    	Logger.log("Parsing finished with errors.");
-	    	return null;
-	    }
 	    else
 	    	Logger.log("Parsing successful.\n");
 	    return tree;
 	}
 
-	//Document parsing is broken, so I have to resort to this. This will later join other similar test cases.
-	public static ParsedStatement giveMeTheTestCase(){
-		ParsedStatement root = new ParsedStatement("placeholder", 0);
-		
-		TheoremStatement theorem1 = new TheoremStatement("$2+3$$$2+5$$",20);
-		theorem1.getChildren().add(new DeclareStatement("{syntax={infix,7,\"/\",l}, meaning=artih1.divide}", 23));
-		theorem1.getChildren().add(new DeclareStatement("{syntax={infix,5,\"+\",l}, meaning=arith1.plus}", 23));
-		theorem1.getChildren().add(new DeclareStatement("{syntax={infix,5,\"-\",l}, meaning=arith1.minus}", 23));
-		theorem1.getChildren().add(new DeclareStatement("{syntax={infix,7,\"*\",l}, meaning=arith1.times}", 23));
-		theorem1.getChildren().add(new DeclareStatement("{macro=\\gcd, meaning=arith1.gcd,    argspec=[2][optional], code={...}}", 80));
-		theorem1.getChildren().add(new FormulaStatement("$2+7$", 23));
-		theorem1.getChildren().add(new FormulaStatement("$$2+5$$", 33));
-		
-		LemmaStatement lemma = new LemmaStatement("placeholder",30);
-		lemma.getChildren().add(new DeclareStatement("{macro=\\gcd, meaning=arith1.gcd,    argspec=[2][optional], code={...}}", 80));
-		lemma.getChildren().add(new DeclareStatement("{macro=\\gcd2, meaning=arith1.gcd2,    argspec=[2][optional], code={...}}", 80));
-
-		//lemma.getChildren().add(new DeclareStatement("\\declare{macro=asd, meaning=asdasd,    argspec=[2], code={\\alright{then}{\\what{is}{\\nesting}}}", 80));
-		lemma.getChildren().add(new FormulaStatement("$$2+5$$", 33));
-		
-		root.getChildren().addAll(Arrays.asList(theorem1,lemma));
-		return root;
-	}
 }
